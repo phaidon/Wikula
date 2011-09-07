26aca4f797e6164a05906e96c59ee9d50c903d4f
Fabian Wuertz <dev@wuertz.org>
2011-09-07 04:57:07 +0200

    Add hyphen2space modifer, refs #43.

---
+++ src/modules/Wikula/templates/plugins/modifier.hyphen2space.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Wikula
+ *
+ * @copyright  (c) Wikula Development Team
+ * @link       https://github.com/phaidon/Wikula/
+ * @license    GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * category    Zikula_3rdParty_Modules
+ * @subpackage Wiki
+ * @subpackage Wikula
+ */
+
+function smarty_modifier_hyphen2space($text)
+{
+    return str_replace('_', ' ', $text);
+}
