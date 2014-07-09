d440a803d2d1c8a5e4a97be1f3e5091bca4338e8
phaidon <bart@Macintosh-2.local>
2014-07-09 18:36:15 +0200

    getlinks -> getLinks

--- src/modules/Wikula/lib/Wikula/Api/Admin.php
+++ src/modules/Wikula/lib/Wikula/Api/Admin.php
@@ -20,7 +20,7 @@ class Wikula_Api_Admin extends Zikula_AbstractApi
      *
      * @return array array of admin links
      */
-    public function getlinks()
+    public function getLinks()
     {
         $links = array();
         if (SecurityUtil::checkPermission('Wikula::', '::', ACCESS_ADMIN)) {
